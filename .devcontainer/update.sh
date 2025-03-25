af33ce3df71387251d188e83dfb9d562de4be859
chris.anatalio <chris.anatalio@stellar.org>
2025-03-25 10:35:04 -0700

    updated config

--- .devcontainer/update.sh
+++ .devcontainer/update.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-brew install stellar-cli
+brew install stellar-cli && brew install cloudentity/tap/oauth2c
 
 echo "Press any key to start your adventure..."
 echo "Welcome to Soroban Quest"
-
-# SSH for remote logs
-ssh -p 2222 -o StrictHostKeyChecking=no -o \
-  UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null vscode@localhost 'nohup command > output.log 2>&1 &'
