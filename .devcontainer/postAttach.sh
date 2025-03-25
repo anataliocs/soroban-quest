990db00e14bc13905ea17cdd5815946df23159db
chris.anatalio <chris.anatalio@stellar.org>
2025-03-25 16:39:46 -0700

    updated config

--- .devcontainer/postAttach.sh
+++ .devcontainer/postAttach.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Terminal style
-apt-get install fonts-powerline
-
 #Stellar auto-complete
 echo "source <(stellar completion --shell bash)" >>"${HOME}"/.bashrc
 echo "source <(stellar completion --shell bash)" >>"${HOME}"/.zshrc
