7af521945b86b0dfc9f31f507ca0272f2bed7f33
chris.anatalio <chris.anatalio@stellar.org>
2025-03-25 11:18:07 -0700

    updated config

--- .devcontainer/update.sh
+++ .devcontainer/update.sh
@@ -7,5 +7,3 @@ echo "Press any key to start your adventure..."
 echo "Welcome to Soroban Quest"
 
 # SSH for remote logs
-ssh -f host xterm -p 2222 -o StrictHostKeyChecking=no -o \
-  UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null vscode@localhost 'nohup command > output.log 2>&1 &'
