55a28be1ea15ea015b1cc09d02d8e7e84f8e06ee
chris.anatalio <chris.anatalio@stellar.org>
2025-03-25 06:22:37 -0700

    updated config

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,5 +5,5 @@ set -e
 bash -i -c 'nvm install --lts'
 git config devcontainers-theme.hide-status 1
 
-
-
+# SSH for remote logs
+ssh -p 2222 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null vscode@localhost
