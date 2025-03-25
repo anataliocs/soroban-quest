b8b692af3ea3d367debfc32f176ef5bb3ca202b3
chris.anatalio <chris.anatalio@stellar.org>
2025-03-25 00:56:09 -0700

    updated config

--- .devcontainer/postAttach.sh
+++ .devcontainer/postAttach.sh
@@ -3,10 +3,8 @@ set -e
 
 # Terminal style
 apt-get install fonts-powerline
-starship preset no-runtime-versions -o ~/.config/starship.toml
-code --install-extension idleberg.emoji-code
 
-
-
-echo "source <(stellar completion --shell bash)" >>~/.bashrc
+#Stellar auto-complete
+echo "source <(stellar completion --shell bash)" >>"${HOME}"/.bashrc
+echo "source <(stellar completion --shell bash)" >>"${HOME}"/.zshrc
 echo "Enabled Stellar CLI auto-completion"
