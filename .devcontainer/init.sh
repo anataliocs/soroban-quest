e0ce58c4d68dadf775d2431c5155a8f934810983
chris.anatalio <chris.anatalio@stellar.org>
2025-03-25 03:40:50 -0700

    updated config

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -28,6 +28,4 @@ apt-get update && export DEBIAN_FRONTEND=noninteractive &&
   curl -sSL https://dl.google.com/linux/direct/google-chrome-stable_current_$(dpkg --print-architecture).deb -o /tmp/chrome.deb &&
   apt-get -y install /tmp/chrome.deb
 
-apt-get update && export DEBIAN_FRONTEND=noninteractive && apt-get install -y firefox-esr
-
 cat "$HOME".fluxbox
