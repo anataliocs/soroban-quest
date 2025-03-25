e9cc743f0510ab7a6c747eb9bc44e8dada4b91eb
chris.anatalio <chris.anatalio@stellar.org>
2025-03-25 04:30:19 -0700

    updated config

--- quests/1-hello-world/src/lib.rs
+++ quests/1-hello-world/src/lib.rs
@@ -4,11 +4,15 @@ use soroban_sdk::{contract, contractimpl, symbol_short, vec, Env, Symbol, Vec};
 //<>QUEST
 // //? Learn about your first Stellar smart contract!
 
-//<>TASK Learn the function of the no_std tag?
+//<>TASK
+// //* Learn the function of the no_std tag
+
 /// The `//![no_std]` attribute in Rust is used to indicate that the crate does not depend
 /// on the standard library, resulting in a smaller binary size.  
 
-//<>TASK Learn about the contract macro
+//<>TASK
+// //* Learn about the contract macro
+
 /// The `//[contract]` attribute in Rust is used to mark a struct as a smart contract. When applied to a
 /// struct, it indicates that the struct represents a smart contract and allows the Soroban SDK to
 /// recognize and interact with it as a contract. This attribute is used to define the contract type and
@@ -17,16 +21,21 @@ use soroban_sdk::{contract, contractimpl, symbol_short, vec, Env, Symbol, Vec};
 #[contract]
 pub struct HelloContract;
 
-//<>TASK Learn about the contract impl macro
+//<>TASK
+// //* Learn about the contract impl macro
+
 /// The `//[contractimpl]` attribute macro in Rust is used to mark an implementation block as the
 /// implementation for a specific contract type. In the context of smart contracts, it is used to define
 /// the functions and behavior associated with a particular contract type. By using `//[contractimpl]`,
 /// you are specifying that the functions within that implementation block are part of the contract
 /// logic and will be attached to the contract type specified by the `//[contract]` attribute macro. This
 /// helps organize and separate the contract logic from other parts of the codebase.
+
 #[contractimpl]
 impl HelloContract {
-    //<>TASK Learn about function declarations
+    //<>TASK
+    // //* Learn about function declarations
+
     /// The `pub fn hello(env: Env, to: Symbol) -> Vec<Symbol>` function in the Rust code snippet is a
     /// public function named `hello` defined within the implementation block of the `HelloContract`
     /// smart contract.
