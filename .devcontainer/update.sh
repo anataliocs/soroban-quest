9080eea64edf86c65f6fc57f66d85b03c902643a
chris.anatalio <chris.anatalio@stellar.org>
2025-03-25 17:17:01 -0700

    updated config

--- .devcontainer/update.sh
+++ .devcontainer/update.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Stellar CLI and oauth2 CLI
-brew install stellar-cli && brew install cloudentity/tap/oauth2c
+brew install stellar-cli && brew install cloudentity/tap/oauth2c && brew install 1password-cli
 
 echo "Press any key to start your adventure..."
 echo "Welcome to Soroban Quest"
