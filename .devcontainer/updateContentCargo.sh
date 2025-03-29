ffeb247ada0955e43031f4090930318ab9f7cc13
chris.anatalio <chris.anatalio@stellar.org>
2025-03-29 15:09:38 -0700

    Update config

--- .devcontainer/updateContentCargo.sh
+++ .devcontainer/updateContentCargo.sh
@@ -2,10 +2,12 @@
 set -e
 
 # Stellar CLI and oauth2 CLI
-brew install stellar-cli && brew install cloudentity/tap/oauth2c
+#brew install cloudentity/tap/oauth2c
+
+cargo install --locked stellar-cli --features opt
 
 echo "Installed Stellar CLI"
-echo "Installed OAuth2 CLI"
+#echo "Installed OAuth2 CLI"
 
 # Check the exit status and provide informative output
 if [ $? -eq 0 ]; then
