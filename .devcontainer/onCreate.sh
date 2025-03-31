32ff86f8b09151ddab15c17eaa4fd38311d4a282
chris.anatalio <chris.anatalio@stellar.org>
2025-03-31 12:22:11 -0700

    Update config

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 set -e
 
-# Install pre-reqs
-sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
-
-# Add Doppler's GPG key
-curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key' | sudo apt-key add -
-
-# Add Doppler's apt repo
-echo "deb https://packages.doppler.com/public/cli/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/doppler-cli.list
-
-# Fetch and install latest doppler cli
-sudo apt-get update && sudo apt-get install -y doppler
-
 # Check the exit status and provide informative output
 if [ $? -eq 0 ] && [ -f "${remoteEnv:BUILD_LOG_FILE}" ]; then
   echo "✅ onCreate() executed successfully" >>"${remoteEnv:BUILD_LOG_FILE}"
