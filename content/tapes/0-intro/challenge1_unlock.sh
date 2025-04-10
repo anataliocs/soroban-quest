335a979ef7957d6e182f7ac75912bc470ad25dae
chris.anatalio <chris.anatalio@stellar.org>
2025-04-10 11:40:06 -0700

    stage for alpha play-test

--- content/tapes/0-intro/challenge1_unlock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sleep 10
-
-echo "CHALLENGE UNLOCKED 🔐"
-exit 0
--- quests/2-auth-store/src/error.rs
+++
@@ -1,9 +0,0 @@
-use soroban_sdk::contracterror;
-
-#[contracterror]
-#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
-#[repr(u32)]
-pub enum ContractError {
-    // Input Value too short, provide at least 11 characters (bytes)
-    InputValueTooShort = 1,
-}
