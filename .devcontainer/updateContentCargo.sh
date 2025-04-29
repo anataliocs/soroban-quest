697811fe1dff39c02b3ef2f20743195ad2788c76
Chris.Anatalio <chris.anatalio@stellar.org>
2025-04-28 23:36:31 -0700

    Fix settings

--- .devcontainer/updateContentCargo.sh
+++ .devcontainer/updateContentCargo.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
 
-cargo install --locked stellar-cli --features opt
-
-echo "Installed Stellar CLI"
-#echo "Installed OAuth2 CLI"
 
 
--- .devcontainer/updateContentDotfile.sh
+++ .devcontainer/updateContentDotfile.sh
@@ -5,16 +5,16 @@ mkdir -p .vscode/
 
 chmod +w ~/.fluxbox
 
-if [ -e .devcontainer/dotfiles/settings.json ]; then
-  mv -n .devcontainer/dotfiles/settings.json .vscode/
+if [ -e .devcontainer/dotfiles/.vscode/settings.json ]; then
+  mv -n .devcontainer/dotfiles/.vscode/settings.json .vscode/
 fi
 
-if [ -e .devcontainer/dotfiles/tasks.json ]; then
-  mv -n .devcontainer/dotfiles/tasks.json .vscode/
+if [ -e .devcontainer/dotfiles/.vscode/tasks.json ]; then
+  mv -n .devcontainer/dotfiles/.vscode/tasks.json .vscode/
 fi
 
-if [ -e .devcontainer/dotfiles/launch.json ]; then
-  mv -n .devcontainer/dotfiles/launch.json .vscode/
+if [ -e .devcontainer/dotfiles/.vscode/launch.json ]; then
+  mv -n .devcontainer/dotfiles/.vscode/launch.json .vscode/
 fi
 
 if [ -e .devcontainer/dotfiles/.fluxbox/menu ]; then
